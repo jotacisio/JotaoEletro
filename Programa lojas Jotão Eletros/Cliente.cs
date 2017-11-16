2ff679a1ddfecadbf7575a8c6445bfac8b6c0651
jotacisio <jotacisio@gmail.com>
2017-11-16 06:20:25 -0300

    initial commit

---
+++ Programa lojas Jotão Eletros/Cliente.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programa_lojas_Jotão_Eletros
+{
+    class Cliente : Principal
+    {
+        //--------------------------------------------------------------------------------------------------------------
+        public void InserirCli()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Cliente objClassCli = new Cliente();
+                //-------------------------------------------------------------------
+                Console.WriteLine("Numero do cartão do cliente é: " + numero);
+                Console.WriteLine("Nome do Cliente: ");
+                objClassCli.nome = Console.ReadLine();
+                Console.WriteLine("CPF do Cliente: ");
+                objClassCli.CPF = Console.ReadLine();
+                Console.WriteLine("RG do Cliente: ");
+                objClassCli.RG = Console.ReadLine();
+                Console.WriteLine("Data de nascimento do Cliente: ");
+                objClassCli.Data = Console.ReadLine();
+                Console.WriteLine("Limite da conta:");
+                objClassCli.limiteconta = Console.ReadLine();
+                Console.WriteLine("Senha do Cliente(de no Mínimo quatro dígitos): ");
+                objClassCli.senha = Console.ReadLine();
+                //-------------------------------------------------------------------
+                ListaCli.Add(objClassCli);
+                //-------------------------------------------------------------------
+                perfumaria();
+                Console.WriteLine();
+                saudacao();
+                Console.WriteLine();
+                perfumaria();
+            }
+            pausaclass();
+        }
+        //--------------------------------------------------------------------------------------------------------------
+        public void BuscarCli()
+        {
+            // nesse método buscar eu queria saber como faz pra ele buscar todos os clientes de mesmo nome ou número
+            //o senhor tinha falado na aula que tinha varias formas de fazer isso mas não encontrei nenhuma
+            //-------------------------------------------------------------------
+            string num;
+            Console.WriteLine("Senha do Cliente: ");
+            num = Console.ReadLine();
+            int AUX = 0;
+            Boolean acheiCli = false;
+            //-------------------------------------------------------------------
+            while ((acheiCli == false) && (AUX < 3))
+            {
+                if (ListaCli[AUX].senha == num)
+                {
+                    acheiCli = true;
+                    perfumaria();
+                    perfumaria();
+                    Console.WriteLine("Nome do Cliente:" + ListaCli[AUX].nome);
+                    Console.WriteLine("CPF do Cliente:" + ListaCli[AUX].CPF);
+                    Console.WriteLine("RG do Cliente:" + ListaCli[AUX].RG);
+                    Console.WriteLine("Data de nascimento do Cliente:" + ListaCli[AUX].Data);
+                    Console.WriteLine("Numero do cartão do Cliente:" + ListaCli[AUX].numero);
+                    Console.WriteLine("Limite da conta:" + ListaCli[AUX].limiteconta);
+                    perfumaria();
+                    perfumaria();
+                }
+                AUX++;
+            }
+            //-------------------------------------------------------------------
+            if (acheiCli == false)
+            {
+                Console.WriteLine("Cliente não encontrado!!!");
+            }
+            //-------------------------------------------------------------------
+        }
+        //--------------------------------------------------------------------------------------------------------------
+        public void ListarCli()
+        {
+            //-------------------------------------------------------------------
+            if (ListaCli.Count == 0)
+            {
+                Console.Beep();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sem clientes inseridos!!!");
+                Console.ResetColor();
+            }
+            //-------------------------------------------------------------------
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    perfumaria();
+                    perfumaria();
+                    Console.WriteLine("Nome do Cliente:" + ListaCli[i].nome);
+                    Console.WriteLine("CPF do Cliente:" + ListaCli[i].CPF);
+                    Console.WriteLine("RG do Cliente:" + ListaCli[i].RG);
+                    Console.WriteLine("Numero do cartão do Cliente:" + ListaCli[i].numero);
+                    Console.WriteLine("Limite da conta:" + ListaCli[i].limiteconta);
+                    perfumaria();
+                    perfumaria();
+                }
+            }
+            //-------------------------------------------------------------------
+           
+        }
+        //--------------------------------------------------------------------------------------------------------------
+        public void SenhaGerente()
+        {
+           string sgerente = "jotao";
+           
+           Console.WriteLine("Inserir senha do gerente(senha ta no método SenhaGerente) :");
+           sgerente = Console.ReadLine();
+           //-------------------------------------------------------------------
+            if (sgerente == "jotao")
+            { 
+                Console.WriteLine("Senha correta!!! ");
+                ListarCli();
+            }
+            else
+            {          
+                   Console.Beep();
+                   Console.ForegroundColor = ConsoleColor.Red;
+                   Console.WriteLine("Digite uma senha valida!!!");
+                   Console.ResetColor();
+            }
+            //-------------------------------------------------------------------
+        }
+        //--------------------------------------------------------------------------------------------------------------
+        public void pausaclass()
+        {
+            int escolha = 0;
+            //-------------------------------------------------------------------
+            while (escolha != 2012)
+            {
+                Console.WriteLine("Deseja substituir os clientes inseridos?");
+                Console.WriteLine("1 - sim");
+                Console.WriteLine("2 - não\n");
+                escolha = int.Parse(Console.ReadLine());
+                switch (escolha)
+                {
+                    //-------------------------------------------------------------------
+                    case 1:
+                        //aki eu to com a mesma dúvida do if logo em baixo
+                        Console.WriteLine();
+                        perfumaria();
+                        Console.WriteLine("A empresa ta crescendo!!!");
+                        perfumaria();
+                        Console.WriteLine();
+                        InserirCli();
+                        break;
+                    //-------------------------------------------------------------------
+                    case 2:
+                        /* aki nesse if eu queria saber como posso fazer pra que ele seja exibido
+                        quando eu criar um dos 3 clientes e não depois dos três como o senhor deve ter observado*/ 
+                        if (escolha != 2012)
+                        {
+                            escolha = 2012;
+                            Console.WriteLine();
+                            perfumaria();
+                            Console.WriteLine("Obrigado pela atenção!!!");
+                            perfumaria();
+                            Console.WriteLine();
+                        }
+                        break;
+                    //-------------------------------------------------------------------
+                    default:
+                        Console.Beep();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine();
+                        Console.WriteLine("Digite um valor valido(1 ou 2)");
+                        Console.WriteLine();
+                        Console.ResetColor();
+                        break;
+                    //-------------------------------------------------------------------
+                }
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------------
+    }
+}
+
---
+++ Programa lojas Jotão Eletros/Servidor.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Programa_lojas_Jotão_Eletros
+{
+    class Servidor : Principal
+    {
+        // Ja que essa classe é praticamente igual a classe Cliente as duvidas são as mesmas 
+        //---------------------------------------------------------------------------------------------------------
+        public void InserirServ()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Servidor objClassServ = new Servidor();
+                //-------------------------------------------------------------------
+                Console.WriteLine("Numero de inscrição do Servidor é: " + numero);
+                Console.WriteLine("Nome do Servidor: ");
+                objClassServ.nome = Console.ReadLine();
+                Console.WriteLine("CPF do Servidor: ");
+                objClassServ.CPF = Console.ReadLine();
+                Console.WriteLine("RG do Servidor: ");
+                objClassServ.RG = Console.ReadLine();
+                Console.WriteLine("Data de nascimento do Servidor: ");
+                objClassServ.Data = Console.ReadLine();
+                Console.WriteLine("Senha do Servidor(de no Mínimo quatro dígitos): ");
+                objClassServ.senha = Console.ReadLine();
+                //-------------------------------------------------------------------
+                ListaServ.Add(objClassServ);
+                //-------------------------------------------------------------------
+                perfumaria();
+                Console.WriteLine();
+                saudacao();
+                Console.WriteLine();
+                perfumaria();
+            }
+            pausaclass();
+        }
+        public void BuscarServ()
+        {
+            string N1;
+            Console.WriteLine("Senha do Servidor: ");
+            N1 = Console.ReadLine();
+            int AUX = 0;
+            Boolean acheiServ = false;
+            //-------------------------------------------------------------------
+            while ((acheiServ == false) && (AUX < 3))
+            {
+                if (ListaServ[AUX].senha == N1)
+                {
+                    acheiServ = true;
+                    perfumaria();
+                    perfumaria();
+                    Console.WriteLine("Nome do Servidor:" + ListaServ[AUX].nome);
+                    Console.WriteLine("CPF do Servidor:" + ListaServ[AUX].CPF);
+                    Console.WriteLine("RG do Servidor:" + ListaServ[AUX].RG);
+                    Console.WriteLine("Data de nascimento do Servidor:" + ListaServ[AUX].Data);
+                    Console.WriteLine("Numero de inscrição do Servidor:" + ListaServ[AUX].numero);
+                    perfumaria();
+                    perfumaria();
+                }
+                AUX++;
+            }
+            //-------------------------------------------------------------------
+            if (acheiServ == false)
+            {
+                Console.WriteLine("Cliente não encontrado!!!");
+            }
+            //-------------------------------------------------------------------
+        }
+        public void ListarServ()
+        {
+            //-------------------------------------------------------------------
+            if (ListaServ.Count == 0)
+            {
+                Console.Beep();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Sem Servidor inseridos!!!");
+                Console.ResetColor();
+            }
+            //-------------------------------------------------------------------
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    perfumaria();
+                    perfumaria();
+                    Console.WriteLine("Nome do Servidor:" + ListaServ[i].nome);
+                    Console.WriteLine("CPF do Servidor:" + ListaServ[i].CPF);
+                    Console.WriteLine("RG do Servidor:" + ListaServ[i].RG);
+                    Console.WriteLine("Numero de inscrição do Servidor:" + ListaServ[i].numero);
+                    perfumaria();
+                    perfumaria();
+                }
+            }
+            //-------------------------------------------------------------------
+        }
+        public void Senhagerente()
+        {
+            string senhaGerente = "jotao";
+
+            Console.WriteLine("Inserir senha do gerente(senha ta no método SenhaGerente) :");
+            senhaGerente = Console.ReadLine();
+            //------------------------------------------------------------------- 
+            if (senhaGerente == "jotao")
+            {
+                Console.WriteLine("Senha correta!!! ");
+                ListarServ();
+            }
+            //-------------------------------------------------------------------
+            else
+            {
+                Console.Beep();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Digite uma senha valida!!!");
+                Console.ResetColor();
+            }
+            //-------------------------------------------------------------------
+        }
+        public void pausaclass()
+        {
+            int escolha = 0;
+            //-------------------------------------------------------------------
+            while (escolha != 2012)
+            {
+                Console.WriteLine("Deseja substituir os servidores inseridos?");
+                Console.WriteLine("1 - sim");
+                Console.WriteLine("2 - não\n");
+                escolha = int.Parse(Console.ReadLine());
+                switch (escolha)
+                {
+                    //-------------------------------------------------------------------
+                    case 1:
+                        Console.WriteLine();
+                        perfumaria();
+                        Console.WriteLine("A empresa ta crescendo!!!");
+                        perfumaria();
+                        Console.WriteLine();
+                        InserirServ();
+                        break;
+                    //-------------------------------------------------------------------
+                    case 2: 
+                        if (escolha != 2012)
+                        {
+                            escolha = 2012;
+                            Console.WriteLine();
+                            perfumaria();
+                            Console.WriteLine("Obrigado pela atenção!!!");
+                            perfumaria();
+                            Console.WriteLine();
+                        }
+                        break;
+                    //-------------------------------------------------------------------
+                    default:
+                        Console.Beep();
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine();
+                        Console.WriteLine("Digite um valor valido(1 ou 2)");
+                        Console.WriteLine();
+                        Console.ResetColor();
+                        break;
+                    //-------------------------------------------------------------------
+                }
+            }
+        }
+    }
+}
+
+
